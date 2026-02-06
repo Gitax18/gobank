83df0762da0888cb851bd7254ee43f191375b97e
Gitax18 <lusifer0418@gmail.com>
2026-02-06 22:49:46 +0530

    add email and hashpassword fields

--- internal/modules/user/user.model.go
+++ internal/modules/user/user.model.go
@@ -7,12 +7,14 @@ import (
 )
 
 type User struct {
-	CreatedAt	  time.Time		`json:"-"`
-	ID            uint    		`gorm:"primarykey;autoIncrement" json:"id"`
-	Name          *string 		`json:"name"`
-	Number        *int    		`gorm:"unique" json:"number"`
-	AccountNumber *int    		`gorm:"unique" json:"account_number"`
-	Balance       *int  		`json:"balance"`
+	CreatedAt	  		time.Time		`json:"-"`
+	ID            		uint    		`gorm:"primarykey;autoIncrement" json:"id"`
+	Email 	  			*string 		`gorm:"unique" json:"email"`
+	HashedPassword	 	*string 		`gorm:"unique" json:"password"`
+	Name          		*string 		`json:"name"`
+	Number        		*int    		`gorm:"unique" json:"number"`
+	AccountNumber 		*int    		`gorm:"unique" json:"account_number"`
+	Balance       		*int	  		`json:"balance"`
 }
 
 func MigrateUser(db *gorm.DB) error{
