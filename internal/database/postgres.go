f8745b2ee0475f9ba2b525ed32f194ad46c70976
Gitax18 <lusifer0418@gmail.com>
2026-02-06 14:42:20 +0530

    add logger to debug db queries

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -6,6 +6,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type Config struct {
@@ -21,7 +22,11 @@ func DBConnection(config *Config) (*gorm.DB, error) {
 	dbcs := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		config.Host, config.Port, config.User, config.Password, config.DBname, config.SSLMode)
 
-	db, err := gorm.Open(postgres.Open(dbcs), &gorm.Config{}); if err != nil{
+	db, err := gorm.Open(postgres.Open(dbcs), &gorm.Config{
+		// add silent so that it shouldn't logs catched error from db
+		// Logger: logger.Default.LogMode(logger.Silent),
+		Logger: logger.Default.LogMode(logger.Error), // default- replace with above to avoid logging handled errors
+	}); if err != nil{
 		log.Fatal(err);
 		return db, err
 	}
