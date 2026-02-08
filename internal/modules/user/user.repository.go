e1f65bfdb2556dc2ea2d8e5c896f8ff40ec5108f
Gitax18 <lusifer0418@gmail.com>
2026-02-08 19:40:27 +0530

    improvise credit and debit logic for concurrency

--- internal/modules/user/user.repository.go
+++ internal/modules/user/user.repository.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type Repository struct {
@@ -59,37 +60,53 @@ func(r* Repository) ReadByMail(email string) (*User, error) {
 
 
 func(r *Repository) Debit(userId int, amount int) error {
-	user := &User{}
+	return r.DB.Transaction(func (tx *gorm.DB) error {
+		user := &User{}
 
-
-	err := r.DB.
-		Where("id = ?", userId).
-		First(user).
-		Error; if err != nil {
+		err := tx.Model(user).
+			Clauses(clause.Locking{Strength: "UPDATE"}).
+			Where("id = ?", userId).
+			First(user).
+			Error; 
+			
+		if err != nil {
 			return errors.New("Error founding user account.\n"+err.Error())
 		}
+		
+		updated_balance := *user.Balance - amount
+		
+		err = tx.Model(&User{}).Where("id=?", userId).Update("balance", updated_balance).Error
+		
+		if err != nil {
+			return errors.New("Error making transaction.\n"+err.Error())
+		}
 
-	updated_balance := *user.Balance - amount
-
-	err = r.DB.Model(&User{}).Where("id=?", userId).Update("balance", updated_balance).Error
-
-	return err
+		return err
+	})
 }
 
 func(r *Repository) Credit(userId int, amount int) error {
-	user := &User{}
+	return r.DB.Transaction(func (tx *gorm.DB) error {
+		user := &User{}
 
-
-	err := r.DB.
-		Where("id = ?", userId).
-		First(user).
-		Error; if err != nil {
+		err := tx.Model(user).
+			Clauses(clause.Locking{Strength: "UPDATE"}).
+			Where("id = ?", userId).
+			First(user).
+			Error; 
+		
+		if err != nil {
 			return errors.New("Error founding user account.\n"+err.Error())
 		}
+		
+		updated_balance := *user.Balance + amount
+		
+		err = tx.Model(&User{}).Where("id=?", userId).Update("balance", updated_balance).Error
+		
+		if err != nil {
+			return errors.New("Error making transaction.\n"+err.Error())
+		}
 
-	updated_balance := *user.Balance + amount
-
-	err = r.DB.Model(&User{}).Where("id=?", userId).Update("balance", updated_balance).Error
-
-	return err
+		return err
+	})
 }
